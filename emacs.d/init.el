bc309ac0aebf0ce311006ed7e0ac6afa0c8ac527
Cedric Mauclair <cmauclai@montcalm.cert.fr>
2011-04-27 08:33:39 +0200

    C-c C-v in context-mode now works ok.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -532,9 +532,10 @@
                    (lambda ()
                      (interactive)
                      (save-buffer)
-                     (async-shell-command
-                      (concat "make -C.. "
-                              (file-name-nondirectory (file-name-sans-extension (buffer-file-name)))))))
+                     (my:compile (concat "make -k "
+                                         (file-name-nondirectory
+                                          (file-name-sans-extension
+                                           (buffer-file-name))) ".pdf"))))
 
     ; insert strings in the buffer)
     (local-set-key (kbd "C-c i")   nil)
@@ -1294,3 +1295,25 @@ found there."
     (my:set-keys (cdr elt) ?' ?')))
 
 ;>> delimiters (end) ---------------------------------------------- >>
+
+(defun push-mark-no-activate ()
+  "Pushes `point' to `mark-ring' and does not activate the region
+Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+  (interactive)
+  (push-mark (point) t nil)
+  (message "Pushed mark to ring"))
+(global-set-key (kbd "C-`") 'push-mark-no-activate)
+
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+(global-set-key (kbd "M-`") 'jump-to-mark)
+
+(defun exchange-point-and-mark-no-activate ()
+  "Identical to \\[exchange-point-and-mark] but will not activate the region."
+  (interactive)
+  (exchange-point-and-mark)
+  (deactivate-mark nil))
+(define-key global-map [remap exchange-point-and-mark] 'exchange-point-and-mark-no-activate)
